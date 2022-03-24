f47e1e0a44b678302255b9551394adfdae90832d
Abdelrhman Elsawy <87781842+abdulrhmanElsawy@users.noreply.github.com>
2022-03-24 09:11:37 +0200

    Update addproduct.php

--- addproduct.php
+++ addproduct.php
@@ -13,13 +13,19 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
             $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
             $price = filter_var($_POST['price'], FILTER_VALIDATE_INT);
 
-            $newItem = new Functions();
-            if ($newItem->checkSku($sku) !== 1) {
-                $newItem -> insertItem($sku, $name, $price, (isset($_POST['size']))? $_POST['size']:null, (isset($_POST['weight']))? $_POST['weight']:null, (isset($_POST['height']))? $_POST['height']:null, (isset($_POST['length']))? $_POST['length']:null, (isset($_POST['width']))? $_POST['width']:null);
-                header('Location: index.php');
+            if(is_numeric($price) && isset($_POST['weight']) && is_numeric($_POST['weight']) || isset($_POST['size']) && is_numeric($_POST['size']) || isset($_POST['height']) && is_numeric($_POST['height']) && isset($_POST['length']) && is_numeric($_POST['length']) && isset($_POST['width']) && is_numeric($_POST['width'])){
+                $newItem = new Functions();
+                if ($newItem->checkSku($sku) !== 1) {
+                    $newItem -> insertItem($sku, $name, $price, (isset($_POST['size']))? $_POST['size']:null, (isset($_POST['weight']))? $_POST['weight']:null, (isset($_POST['height']))? $_POST['height']:null, (isset($_POST['length']))? $_POST['length']:null, (isset($_POST['width']))? $_POST['width']:null);
+                    header('Location: index.php');
+                }else{
+                    echo "<div class='error-msg '><h2 class='alert alert-danger'>sorry this sku is exist try another one</h2></div>";
+                }
             }else{
-                echo "<div class='error-msg '><h2 class='alert alert-danger'>sorry this sku is exist try another one</h2></div>";
+                echo "<div class='error-msg '><h2 class='alert alert-danger'>Data must be in the correct way</h2></div>";
+
             }
+
 
         }else{
             echo "<div class='error-msg '><h2 class='alert alert-danger'>Sorry all fields are required</h2></div>";
